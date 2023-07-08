46697737602e6d59f420424b887767585c2766d9
daothikhuyen <daothikhuyen30@gmail.com>
2023-07-08 14:56:13 +0700

    DO_An_Co_So_1

---
+++ Do_An_Co_So1/assets/javscript/script.js
@@ -0,0 +1,29 @@
+    
+    var getSearchIcon = document.querySelector('.btn-seacrch');
+    var getSearchTyping = document.getElementById('search-input');
+    var searchToggle = false;
+    getSearchTyping.style.display='none';
+    getSearchIcon.addEventListener('click',function(){
+        if(!searchToggle){
+            getSearchTyping.style.display='block';
+            searchToggle = ! searchToggle;
+        }
+        else{
+            getSearchTyping.style.display='none';
+            searchToggle = ! searchToggle;
+        }
+    });
+
+    var getBtn = document.querySelector('.btnSeeAll');
+    var getSeeAll = document.getElementById('SP-See-all');
+    var SeeToggle = false;
+    getSeeAll.style.display='none';
+    getBtn.addEventListener('click', function(){
+        if(!SeeToggle){
+            getSeeAll.style.display='block';
+            SeeToggle = ! SeeToggle;
+            getBtn.style.display='none';
+        }
+    });
+
+      
